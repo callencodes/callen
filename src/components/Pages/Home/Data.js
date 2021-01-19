d93c7dcd2f86a2e747f38ecccabc45e349e5c125
cal <callencoding@gmail.com>
2021-01-18 20:06:44 -0500

    updated data

--- src/components/Pages/Home/Data.js
+++ src/components/Pages/Home/Data.js
@@ -2,11 +2,36 @@ export const homeObjUno = {
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
-  topLine: 'Exclusive Access',
-  headline: 'Unlimited Transactions with zero fees',
-  description: 'This is a description full of words that I hope look good on this page',
+  topLine: '',
+  headline: 'Software Developer',
   buttonLabel: 'Portfolio',
   imgStart: '',
   img: '/public/images/holder.png',
-  alt: 'logo'
+  alt: 'logo',
+  name: "Chloe Allen",
+  occupation: "software developer",
+  languages: "Python, Node.js, React, SQL",
+  technologies: "AWS, Docker",
+  frameworks: "Django, Flask, Express",
+  description: "recently graduated university with a B.S",
+  email: "chloe@chloeallen.io",
+  country: "United States",
+  website: "http://www.chloeallen.io",
+  "social": [
+    {
+      "name": "linkedin",
+      "url": "www.linkedin.com/in/chloe-allen-596a64180",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/callencodes",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "twitter",
+      "url": "https://twitter.com/callencodes",
+      "className": "fa fa-twitter"
+    }
+  ]
 }
